45be8c319336a48c73dbe2a8fe54ff8b7b20e671
rohankar7 <rohankar365@gmail.com>
2024-09-20 11:42:03 +0100

    Deleting files

--- viewer.py
+++
@@ -1,7 +0,0 @@
-import os
-from file_address import *
-import trimesh
-import numpy as np
-# file_path = 'C:/ShapeNetCore/02691156/10155655850468db78d106ce0a280f87/models/model_normalized.obj'
-# mesh = trimesh.load('./generated_models/02747177_10839d0dc35c94fcf4fb4dee5181bee_rotated.ply', force='mesh')
-# mesh.show()
